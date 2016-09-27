04aad9e8f579a3c0bf1cb43a59b3fc8fe79c3f1b
Thomas Minier <tminier01@gmail.com>
2016-09-27 13:19:14 +0200

    add monitor waiting line for Server + fix Maven not targetting java 1.8

--- src/main/java/com/alma/pay2bid/Client.java
+++ src/main/java/com/alma/pay2bid/Client.java
@@ -1,16 +1,22 @@
 package com.alma.pay2bid;
 
 import com.alma.pay2bid.common.IClient;
+import com.alma.pay2bid.common.IServer;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Logger;
 
 /**
  * Created by E122371M on 27/09/16.
  */
 public class Client extends UnicastRemoteObject implements IClient {
 
-    protected Client() throws RemoteException {
+    private static final Logger LOGGER = Logger.getLogger("com.alma.pay2bid.Client.logger");
+    private IServer server;
+
+    public Client(IServer server) throws RemoteException {
+        this.server = server;
     }
 
     public void submit(Auction auction) throws RemoteException {
--- src/main/java/com/alma/pay2bid/Server.java
+++ src/main/java/com/alma/pay2bid/Server.java
@@ -5,17 +5,36 @@ import com.alma.pay2bid.common.IServer;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Logger;
 
 /**
  * Created by E122371M on 27/09/16.
  */
 public class Server extends UnicastRemoteObject implements IServer {
 
-    protected Server() throws RemoteException {
+    private static final Logger LOGGER = Logger.getLogger("com.alma.pay2bid.Server.logger");
+    private List<IClient> clients = new ArrayList<>();
+    private Queue<Auction> auctions = new ConcurrentLinkedQueue<>();
+
+    public Server() throws RemoteException {
+
     }
 
-    public void register(IClient client) throws RemoteException {
+    public synchronized void register(IClient client) throws RemoteException {
+        try {
+            wait();
+            clients.add(client);
+        } catch (InterruptedException e) {
+            LOGGER.warning(e.getMessage());
+        }
+    }
 
+    private synchronized void validateRegistrations() {
+        notifyAll();
     }
 
     public void raise_bid(IClient client, int new_bid) throws RemoteException {
