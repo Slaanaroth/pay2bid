aae0b53241b5c3f0058ab9bbb7f4f7e42a60b0cc
Rintarou <merlin.barzilai@gmail.com>
2018-11-04 20:38:51 +0100

    adding description and removing timers after a win

--- src/main/java/com/alma/pay2bid/gui/AuctionView.java
+++ src/main/java/com/alma/pay2bid/gui/AuctionView.java
@@ -19,16 +19,17 @@ public class AuctionView {
     private JPanel auctionPanel;
     private JLabel auctionPriceValue;
     private JLabel auctionTimer;
+    private JLabel auctionTimerLabel;
     private JLabel auctionCurrentWinnerLabel;
     private JLabel auctionCurrentWinnerValue;
-    private JLabel auctionDescriptionValue;
+    private JTextArea auctionDescriptionValue;
     private JTextField auctionBid;
     private JLabel auctionBidLabel;
     private JButton raiseButton;
 
     public AuctionView(AuctionBean auction){
         auctionPanel = new JPanel();
-        auctionPanel.setMaximumSize(new Dimension(500, 200));
+        auctionPanel.setMaximumSize(new Dimension(500, 300));
         auctionPanel.setLayout(new GridLayout(7, 3, 5, 5));
 
         // Create the price label
@@ -41,9 +42,8 @@ public class AuctionView {
 
         // Create the price description
         JLabel auctionDescriptionLabel = new JLabel(" Description : ");
-        auctionDescriptionValue = new JLabel("");
-        auctionDescriptionValue.setText(auction.getDescription());
-        auctionDescriptionValue.setLabelFor(auctionDescriptionLabel);
+        auctionDescriptionValue = new JTextArea(auction.getDescription(),10,18);
+        auctionDescriptionValue.setLineWrap(true);
         auctionPanel.add(auctionDescriptionLabel);
         auctionPanel.add(auctionDescriptionValue);
 
@@ -57,7 +57,7 @@ public class AuctionView {
 
         //Create the timer label
         auctionTimer = new JLabel("0");
-        JLabel auctionTimerLabel = new JLabel("Remaining time : ");
+        auctionTimerLabel = new JLabel("Remaining time : ");
         auctionTimer.setLabelFor(auctionTimerLabel);
         auctionPanel.add(auctionTimerLabel);
         auctionPanel.add(auctionTimer);
@@ -82,6 +82,8 @@ public class AuctionView {
         auctionBid.setVisible(true);
         auctionBidLabel.setVisible(true);
         raiseButton.setVisible(true);
+        auctionTimer.setVisible(true);
+        auctionTimerLabel.setVisible(true);
     }
 
     public void disable() {
@@ -89,6 +91,8 @@ public class AuctionView {
         auctionBid.setVisible(false);
         auctionBidLabel.setVisible(false);
         raiseButton.setVisible(false);
+        auctionTimer.setVisible(false);
+        auctionTimerLabel.setVisible(false);
     }
 
     public void setWinner(String name) {
@@ -102,6 +106,8 @@ public class AuctionView {
         auctionCurrentWinnerValue.setVisible(false);
         auctionBid.setVisible(false);
         raiseButton.setVisible(false);
+        auctionTimer.setVisible(false);
+        auctionTimerLabel.setVisible(false);
     }
 
     public void setRaiseButton(JButton raiseButton) {
